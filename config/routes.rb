0088387034acb5cb24e53ae565b801600f998fef
Matthew Anderson <manderson@onehub.com>
2009-02-02 22:05:05 -0800

    Routing root to quips

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :quips
 
-  map.resources :quips
-
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -37,7 +35,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "quips"
 
   # See how all your routes lay out with "rake routes"
 
