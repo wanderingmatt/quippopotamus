523859771556022aac340798df4e65fe380f9ada
Matthew Anderson <manderson@onehub.com>
2009-02-02 22:45:37 -0800

    Updated user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,36 @@ class UsersController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
   
+  # GET /users
+  # GET /users.xml
+  def index
+    @users = User.find(:all, :order => :login)
 
-  # render new.rhtml
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @users }
+    end
+  end
+
+  # GET /users/1
+  # GET /users/1.xml
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @user }
+    end
+  end
+
+  # GET /users/new
+  # GET /users/new.xml
   def new
+  end
+  
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
